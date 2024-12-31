4fbf78fd6736d1f427b24582f99ef3904657650f
ylair <lair.yamila.belen@gmail.com>
2024-12-31 14:57:47 -0300

    feat: titulo

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,13 +5,15 @@ export default function Header() {
     <div className="w-full flex flex-col md:flex-row gap-5 h-full items-center justify-center">
       <div
         className="w-52 h-52 md:w-[300px] md:h-[300px] bg-cover bg-top rounded-full"
-        style={{ backgroundImage: "url(./bel.jpg)" }}
+        style={{ backgroundImage: "url(./images/bel.jpg)" }}
       ></div>
       <div className="flex flex-col">
-        <p className="italic text-lg md:text-xl font-thin">YAMILA BELEN LAIR</p>
-        <p className="font-semibold text-2xl md:text-3xl text-active">
+        <h1 className="italic text-lg md:text-xl font-thin">
+          YAMILA BELEN LAIR
+        </h1>
+        <h2 className="font-semibold text-2xl md:text-3xl text-active">
           FRONTEND DEVELOPER
-        </p>
+        </h2>
       </div>
     </div>
   );
